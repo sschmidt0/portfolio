146c72bc599ea5ab84e785afa09046ec5d7cd210
sarah <sschmidt0@uoc.edu>
2021-09-04 19:25:04 +0200

    added intersection observer for contact animation

--- components/composed/ContactComponent/ContactInfo.js
+++ components/composed/ContactComponent/ContactInfo.js
@@ -1,14 +1,38 @@
+import { useEffect } from 'react';
 import { FaGithub, FaLinkedin, FaTwitter } from 'react-icons/fa';
 import styles from '../../../styles/Contacto.module.scss';
+import { motion, useAnimation } from "framer-motion";
+import { fadeInUp } from '../../../assets/animations/animations';
+import { useInView } from 'react-intersection-observer';
 
-export const ContactInfo = () => (
-  <div className={ styles.infobox }>
-    <span>Sarah Schmidt</span>
-    <span>sschmidt0@uoc.edu</span>
-    <div>
-      <a href="https://github.com/sschmidt0" target="_blank" rel="noreferrer"><FaGithub /></a>
-      <a href="https://www.linkedin.com/in/sschmidt0/" target="_blank" rel="noreferrer"><FaLinkedin /></a>
-      <a href="https://twitter.com/sschmidt0" target="_blank" rel="noreferrer"><FaTwitter /></a>
-    </div>
-  </div>
-);
+export const ContactInfo = () => {
+  const controls = useAnimation();
+  const { ref, inView } = useInView();
+
+  useEffect(() => {
+    if (inView) {
+      controls.start('animate');
+    }
+    if (!inView) {
+      controls.start('initial');
+    }
+  }, [controls, inView]);
+
+  return (
+    <motion.div
+      className={ styles.infobox }
+      initial="initial"
+      animate={ controls }
+      variants={ fadeInUp }
+      ref={ ref }
+    >
+      <span>Sarah Schmidt</span>
+      <span>sschmidt0@uoc.edu</span>
+      <div>
+        <a href="https://github.com/sschmidt0" target="_blank" rel="noreferrer"><FaGithub /></a>
+        <a href="https://www.linkedin.com/in/sschmidt0/" target="_blank" rel="noreferrer"><FaLinkedin /></a>
+        <a href="https://twitter.com/sschmidt0" target="_blank" rel="noreferrer"><FaTwitter /></a>
+      </div>
+    </motion.div>
+  );
+};
