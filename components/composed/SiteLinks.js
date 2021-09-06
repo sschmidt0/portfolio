f9fdefe3dffa16a75c081bd7884d5d6f9323b5da
sarah <sschmidt0@uoc.edu>
2021-09-06 16:34:56 +0200

    changed link text to contact page

--- components/composed/SiteLinks.js
+++ components/composed/SiteLinks.js
@@ -5,7 +5,7 @@ export const SiteLinks = ({ site }) => {
   const router = useRouter();
   const linkCV = router.locale === 'en' ? 'Want more? Check out my CV.' : '¿Ganas de más? Mira mi CV.';
   const linkPortfolio = router.locale === 'en' ? 'Want more? Check out my portfolio.' : '¿Ganas de más? Mira mi portafolio.';
-  const linkContact = router.locale === 'en' ? "Or why don't you get in contact with me?" : '¿O por qué no te pones en contacto conmigo?';
+  const linkContact = router.locale === 'en' ? "Want to get in touch with me?" : '¿Quieres ponerte en contacto conmigo?';
 
   return (
     <div className="siteLinks">
